2da2f2e56ac70a1127648dccb3db81bd01932077
izayl <izayl@163.com>
2023-04-30 02:00:55 +0800

    fix: move seo config to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,38 +5,8 @@ import { Toaster } from '@/components/ui/toaster'
 import { SiteHeader } from '@/components/site-header'
 import { SiteFooter } from '@/components/site-footer'
 import { cn } from '@/lib/utils'
-import { siteConfig } from '@/config'
-import { Metadata } from 'next/types'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: siteConfig.name,
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
-  icons: '/favicon.svg',
-  authors: [
-    {
-      name: siteConfig.author,
-      url: siteConfig.authorUrl,
-    },
-  ],
-  openGraph: {
-    title: siteConfig.name,
-    description: siteConfig.description,
-    type: 'website',
-    images: '/images/intro.png',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteId: '3049993370',
-    creator: '@izayl_',
-    creatorId: '3049993370',
-    images: ['/images/intro.png'],
-  },
-}
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 import { Result } from '@/components/result'
 import { SearchInput } from '@/components/search-input'
+import { siteConfig } from '@/config'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  icons: '/favicon.svg',
+  authors: [
+    {
+      name: siteConfig.author,
+      url: siteConfig.authorUrl,
+    },
+  ],
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    type: 'website',
+    images: '/images/intro.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteId: '3049993370',
+    creator: '@izayl_',
+    creatorId: '3049993370',
+    images: ['/images/intro.png'],
+  },
+}
 export default function Home() {
   return (
     <main className="container max-w-2xl p-0">
